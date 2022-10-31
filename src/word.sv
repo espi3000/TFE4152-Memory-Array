78437004397f41971718ea4f22242bdc330914c4
espi3000 <espen.holsen.eh@gmail.com>
2022-10-31 09:34:14 +0100

    Tried to make a byte

---
+++ src/word.sv
@@ -0,0 +1,12 @@
+module word(
+    input [7:0] in, 
+    output [7:0] out, 
+    input sel, 
+    input rw
+    );
+    generate
+        for (i=0; i<8; i=i+1) begin
+            bitcell_nor bitcell(.out(out[i]), .in(in[i]), .sel(sel), .rw(rw));
+        end
+    endgenerate
+endmodule
